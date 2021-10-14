afe3f5b390ab1b4f87a63c839af5536ff00e5953
adultlee <seoungin1228@gmail.com>
2021-10-14 19:07:40 +0900

    write : 작성

---
+++ js/기초/출력/HelloWorld.js
@@ -0,0 +1,22 @@
+console.log("hello world!");
+
+console.log("나다");
+
+const printf = (arg) =>
+{
+
+    console.log(arg);
+
+    console.log(arg);
+}
+
+// const printff(arg) 
+// {
+
+//     console.log(arg);
+
+//     console.log(arg);
+// }
+
+printf('hihi');
+// printff("이건달라");
