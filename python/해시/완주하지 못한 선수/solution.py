e59c0b0c34e0d551e37f4fb77425644be615e311
adultlee <seoungin1228@gmail.com>
2021-12-11 23:38:15 +0900

    feat: add problem

--- python/해시/완주하지 못한 선수/solution.py
+++ python/해시/완주하지 못한 선수/solution.py
@@ -1,22 +1,20 @@
 def solution(participant, completion):
     participant.sort()
     completion.sort()
+    print(participant)
+    print(completion)
+    arr = zip(participant, completion)
+    print(arr)
     for p,c in zip(participant, completion):
         print(p)
         print(c)
         if p != c:
+            print(p)
             return p
     return participant.pop()
 
 arr = ["abbbbbb","acb",3,2,4]
 arr2 = ["abbbbbb","acb",3]
-
-# def sortt(arr):
-#     print(arr)
-#     arr.sort()
-#     print(arr)
-
-# sortt(arr)
 
 solution(arr,arr2)
 
