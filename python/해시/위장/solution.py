1d23c4b29f19ee2f1928bb78afa02ed0dceaa520
adultlee <seoungin1228@gmail.com>
2021-12-19 23:25:04 +0900

    solve:  위장 문제 풀이

--- python/해시/위장/solution.py
+++ python/해시/위장/solution.py
@@ -1,3 +1,6 @@
 def solution(clothes):
-    answer = 0
+    from collections import Counter
+    from functools import reduce
+    cnt = Counter([kind for name, kind in clothes])
+    answer = reduce(lambda x, y: x*(y+1), cnt.values(), 1) - 1
     return answer
