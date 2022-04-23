4239b306d67ab83af9ac5d3e021e8b085d530504
adultlee <seoungin1228@gmail.com>
2022-04-24 01:55:12 +0900

    solve 숫자 문자열과 영단어

--- js/1단계/숫자 문자열과 영단어/solution.js
+++ js/1단계/숫자 문자열과 영단어/solution.js
@@ -1,22 +1,22 @@
 function solution(s) {
-    var answer = 0;
-    s = s.replace(/zero/g, '0');
-    s = s.replace(/one/g, '1');
-    s = s.replace(/two/g, '2');
-    s = s.replace(/three/g, '3');
-    s = s.replace(/four/g, '4');
-    s = s.replace(/five/g, '5');
-    s = s.replace(/six/g, '6');
-    s = s.replace(/seven/g, '7');
-    s = s.replace(/eight/g, '8');
-    answer = s.replace(/nine/g, '9');
+  var answer = 0;
+  s = s.replace(/zero/g, "0");
+  s = s.replace(/one/g, "1");
+  s = s.replace(/two/g, "2");
+  s = s.replace(/three/g, "3");
+  s = s.replace(/four/g, "4");
+  s = s.replace(/five/g, "5");
+  s = s.replace(/six/g, "6");
+  s = s.replace(/seven/g, "7");
+  s = s.replace(/eight/g, "8");
+  answer = s.replace(/nine/g, "9");
 
-    answer = Number(answer)
-    return answer;
+  answer = Number(answer);
+  return answer;
 }
 
-var answer =0;
-var s = "one4seveneight"
+var answer = 0;
+var s = "one4seveneight";
+
 solution(s);
-console.log(solution(s)
-);
+console.log(solution(s));
---
+++ js/1단계/숫자 문자열과 영단어/solution1.js
@@ -0,0 +1,27 @@
+function solution(s) {
+  let numbers = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+  var answer = s;
+
+  for (let i = 0; i < numbers.length; i++) {
+    let arr = answer.split(numbers[i]);
+    // console.log(i);
+    // console.log(arr);
+    answer = arr.join(i);
+  }
+
+  return Number(answer);
+}
+var s = "one4seveneight";
+
+solution(s);
