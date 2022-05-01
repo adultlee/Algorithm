ce11550d05e11015f331d07a0f0dbd8af2c1311a
adultlee <seoungin1228@gmail.com>
2022-05-01 19:37:54 +0900

    solve: 뉴스 클러스터링

--- js/2단계/뉴스 클러스터링/solve1.js
+++ js/2단계/뉴스 클러스터링/solve1.js
@@ -24,9 +24,6 @@ function solution(str1, str2) {
       arr2.push(str2[i] + str2[i + 1]);
   }
 
-  console.log(arr1);
-  console.log(arr2);
-
   //교집합 , 합집합 구하기
   let inner = []; // 교집합
   let sum = [...arr1, ...arr2]; // 합집합
@@ -39,11 +36,19 @@ function solution(str1, str2) {
       return false;
     }
   });
-  for (let i = 0; i < inner.length; i++) {
-    if (sum.includes(inner[i])) {
-      sum.splice(sum.indexOf(inner[i]), 1);
+  let temp1 = [...inner];
+  sum.map((x) => {
+    console.log("x is " + x);
+    console.log("sum is " + sum);
+    console.log("temp is " + temp1);
+    if (temp1.includes(x)) {
+      temp1.splice(temp1.indexOf(x), 1);
+      sum.splice(sum.indexOf(x), 1);
+      console.log("after sum is " + sum);
+    } else {
     }
-  }
+  });
+  console.log(temp1);
   console.log(sum);
   console.log(inner);
 
@@ -53,6 +58,11 @@ function solution(str1, str2) {
 
   return Math.floor((inner.length / sum.length) * 65536);
 }
+
+var str1 = "aaaab";
+var str2 = "aaab";
+
+solution(str1, str2);
 // 두 문자열을 받아서 대문자로 바꾸어 준다.
 // 두 글자씩 입력 받을때 예외처리를 하여 영어만 받을 수 있도록 한다.
 // 교집합을 구한다.
